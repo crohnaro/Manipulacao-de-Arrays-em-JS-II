968367c99bde8bed3f5bf1772980b1707b1ef4cd
Welker42 <welkerandrade@gmail.com>
2024-06-21 16:22:46 -0300

    Solução de Exercícios

--- exercises/combinacoes.js
+++ exercises/combinacoes.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 function combinacoes(array1, array2) {
     let result = [];
    
@@ -13,13 +12,3 @@ function combinacoes(array1, array2) {
 }
  
 module.exports = combinacoes; // Para Node.js/CommonJS
-=======
-function combinacoes(array1, array2) {}
-
-// Testes
-console.log(combinacoes([1, 2], ["a", "b"])); // [[1, 'a'], [1, 'b'], [2, 'a'], [2, 'b']]
-console.log(combinacoes([1], ["a", "b", "c"])); // [[1, 'a'], [1, 'b'], [1, 'c']]
-console.log(combinacoes([], [1, 2])); // []
-
-modoule.exports = combinacoes;
->>>>>>> e37f5977e00adb476d937075880cb1ac23069d07
