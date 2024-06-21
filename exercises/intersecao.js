298423aacae147145bb2fe4c68dbb8be87bc2c73
ArthurFSC <arthurfellps456@gmail.com>
2024-06-21 16:27:38 -0300

    Solução dos Exercícios

--- exercises/intersecao.js
+++ exercises/intersecao.js
@@ -1,10 +1,19 @@
 /* Descrição: Crie uma função chamada interseccao que recebe dois arrays e retorna um novo array contendo apenas os elementos que estão presentes em ambos os arrays.*/
 
 function interseccao(array1, array2) {
-  //implemente seu codigo aqui
-}
-
-// Testes
-console.log(interseccao([1, 2, 3], [2, 3, 4])); // [2, 3]
-console.log(interseccao([1, 2, 3], [4, 5, 6])); // []
-console.log(interseccao(["a", "b", "c"], ["b", "c", "d"])); // ['b', 'c']
+  let resultado = [];
+  
+  for (let i = 0; i < array1.length; i++) {
+  if (array2.includes(array1[i])) {
+  resultado.push(array1[i]);
+  }
+  }
+  
+  return resultado;
+  }
+  // Testes
+  console.log(interseccao([1, 2, 3], [2, 3, 4])); // [2, 3]
+  console.log(interseccao([1, 2, 3], [4, 5, 6])); // []
+  console.log(interseccao(["a", "b", "c"], ["b", "c", "d"])); // ['b', 'c']
+  
+  module.exports = interseccao;
--- exercises/ordernar.js
+++ exercises/ordernar.js
@@ -1,8 +1,13 @@
 /*Descrição: Crie uma função chamada ordenarPorComprimento que recebe um array de strings e retorna o array ordenado pelas strings de menor para maior comprimento. */
 
-function ordenarPorComprimento(array) {}
-
-// Testes
-console.log(ordenarPorComprimento(["apple", "banana", "kiwi", "cherry", "grape"])); // ["kiwi", "apple", "grape", "cherry", "banana"]
-console.log(ordenarPorComprimento(["a", "abc", "ab"])); // ["a", "ab", "abc"]
-console.log(ordenarPorComprimento([])); // []
+function ordenarPorComprimento(array) {
+    return array.sort((a, b) => a.length - b.length);
+    }
+    
+    // Testes
+    console.log(ordenarPorComprimento(["apple", "banana", "kiwi", "cherry", "grape"])); // ["kiwi", "apple", "grape", "cherry", "banana"]
+    console.log(ordenarPorComprimento(["a", "abc", "ab"])); // ["a", "ab", "abc"]
+    console.log(ordenarPorComprimento([])); // []
+    
+    
+    module.exports = ordenarPorComprimento;
