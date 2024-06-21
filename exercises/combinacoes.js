40032e47c959ef5256bf6a4e6dd5483da936e48c
Akaren25 <karenkrishna225@gmail.com>
2024-06-21 14:47:54 -0300

    Solução dos execícios

--- exercises/combinacoes.js
+++ exercises/combinacoes.js
@@ -1,10 +1,20 @@
-/*Descrição: Crie uma função chamada combinacoes que recebe dois arrays e retorna um array de todas as combinações possíveis de elementos desses arrays. */
+/* Descrição: Crie uma função chamada combinacoes que recebe dois arrays 
+ * e retorna um array de todas as combinações possíveis de elementos desses arrays.
+ */
 
-function combinacoes(array1, array2) {}
+function combinacoes(array1, array2) {
+    let combinacoes = [];
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            combinacoes.push([array1[i], array2[j]]);
+        }
+    }
+    return combinacoes;
+}
 
 // Testes
 console.log(combinacoes([1, 2], ["a", "b"])); // [[1, 'a'], [1, 'b'], [2, 'a'], [2, 'b']]
 console.log(combinacoes([1], ["a", "b", "c"])); // [[1, 'a'], [1, 'b'], [1, 'c']]
 console.log(combinacoes([], [1, 2])); // []
 
-modoule.exports = combinacoes;
+module.exports = combinacoes;
