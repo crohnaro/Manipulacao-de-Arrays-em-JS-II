35552f1f0fe0e9784ffaf618d2023939b71fadad
Augusto-vitor <vitorgt7oliveira@gmail.com>
2024-06-24 16:44:29 -0300

    Solução dos exercícios

--- tests/ordernar.test.js
+++ tests/ordernar.test.js
@@ -1,13 +1,16 @@
-const { ordenarPorComprimento } = require('../exercises/ordernar');
-
-test('ordenar array de strings ["apple", "banana", "kiwi", "cherry", "grape"] por comprimento', () => {
-  expect(ordenarPorComprimento(["apple", "banana", "kiwi", "cherry", "grape"])).toEqual(["kiwi", "apple", "grape", "cherry", "banana"]);
+// Corrija o caminho de importação se necessário
+const ordernarPorComprimento = require('../exercises/ordernar');
+ 
+test('ordernar array de strings ["apple", "banana", "kiwi", "cherry", "grape"] por comprimento', () => {
+    const input = ["apple", "banana", "kiwi", "cherry", "grape"];
+    const outputEsperado = ["kiwi", "apple", "grape", "banana", "cherry"];
+    expect(ordernarPorComprimento(input)).toEqual(outputEsperado);
 });
-
+ 
 test('ordenar array de strings ["a", "abc", "ab"] por comprimento', () => {
-  expect(ordenarPorComprimento(["a", "abc", "ab"])).toEqual(["a", "ab", "abc"]);
+    expect(ordernarPorComprimento(["a", "abc", "ab"])).toEqual(["a", "ab", "abc"]);
 });
-
+ 
 test('ordenar array vazio', () => {
-  expect(ordenarPorComprimento([])).toEqual([]);
+    expect(ordernarPorComprimento([])).toEqual([]);
 });
